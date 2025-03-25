28badfa17875dfa080aedf91c716b97d7f75178f
yhlas <wk.styhlas21@gmail.com>
2025-03-25 11:29:14 +0300

    form added

--- loginpage/index.js
+++ loginpage/index.js
@@ -1,1 +1,15 @@
 //You don't need this, but wait what if you wanna change the theme
+document.getElementById('signform').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const formData = new FormData(this);
+  
+  const formValues = Object.fromEntries(formData.entries());
+  
+  console.log('Form data:', formValues);
+  
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+});
+
