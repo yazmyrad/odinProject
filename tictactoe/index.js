8cee891a2e6314f240fa08bce484033e161a9bfe
yhlas <wk.styhlas21@gmail.com>
2025-04-13 16:36:57 +0300

    Score display and reset button added

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -3,30 +3,35 @@ let board = [null, null, null, null, null, null, null, null, null];
 let gamestatus = false;
 let horizontal = [0, 3, 6];
 let vertical   = [0, 1, 2];
+let userscore  = [0, 0];
 
 function checkGame(turn) {    
     for(let i of horizontal){
 	    if ((board[i] !== null) && (board[i] === board[i+1]) && (board[i+1] === board[i+2])) {
 		gamestatus = true;
-		console.log(`Player ${(turn % 2) ? 0 : 1} wins!`);
+		alert(`Player ${(turn % 2) ? 0 : 1} wins!`);
+		userscore[(turn % 2) ? 0 : 1] += 1;
 	    }
     }
     
     for(let i of vertical){
 	    if ((board[i] !== null) && (board[i] === board[i+3]) && (board[i+3] === board[i+6])) {
 		gamestatus = true;
-		console.log(`Player ${(turn % 2) ? 0 : 1} wins!`);
+		alert(`Player ${(turn % 2) ? 0 : 1} wins!`);
+		userscore[(turn % 2) ? 0 : 1] += 1;
 	    }
     }
     
     if ((board[0] !== null) && (board[0] === board[4]) && (board[4] === board[8])) {
 	gamestatus = true;
-	console.log(`Player ${(turn % 2) ? 0 : 1} wins!`);
+	alert(`Player ${(turn % 2) ? 0 : 1} wins!`);
+	userscore[(turn % 2) ? 0 : 1] += 1;
   	return gamestatus;
     }
     if ((board[2] !== null) && (board[2] === board[4]) && (board[4] === board[6])) {
 	gamestatus = true;
-	console.log(`Player ${(turn % 2) ? 0 : 1} wins!`);
+	alert(`Player ${(turn % 2) ? 0 : 1} wins!`);
+	userscore[(turn % 2) ? 0 : 1] += 1;
   	return gamestatus;
     }
     return gamestatus;    
@@ -35,35 +40,46 @@ function checkGame(turn) {
 let user = 0;
 let turn = 0;
 function game(num) {
-
-	if (board[num] !== null) {
-	    console.log('Cell already taken! Choose again.');
-	    return false;
-	}
-
 	board[num] = user;
 
 	gamestatus = checkGame(turn);
 
 	user = (user + 1) % 2;
-    
-    if (!gamestatus) {
-        console.log("It's a draw!");
-    }
-    turn +=1;
-    
-    return true;
+    	turn +=1;
+    	return gamestatus;
 }
 
-document.querySelectorAll('button').forEach(button => { button.addEventListener( "click", () => {
-            const fired_button = parseInt(button.value);
-            button.style.backgroundImage = `url(${user}.png)`;
-            let output = game(fired_button);
-            if (output === false)
-                alert("Already taken");
-            
-            
-        }
-    );
-});
-//game().then(end => console.log("Game ended:", end));
+let score = document.querySelector('.score');
+let gameboard = document.querySelector('.gameboard');
+function play(button){
+    const fired_button = parseInt(button.value);
+    if (board[fired_button] !== null) {
+	    alert('Cell already taken! Choose again.');
+    }else {
+    	button.style.backgroundImage = `url(${user}.png)`;
+	let output = game(fired_button);
+	if(output === true || (output === false && turn === 8)){
+		board = [null, null, null, null, null, null, null, null, null];
+		turn = 0;
+		user = 0;
+		if (output === false && turn === 8){alert("It's draw!");};
+		document.querySelectorAll('button').forEach(button => { button.style.backgroundImage = '';});
+		score.textContent = `${userscore[0]} - ${userscore[1]}`;
+		gamestatus = false;
+		return true;
+	}
+    }            
+}
+
+function reset(){
+	userscore = [0, 0];
+	board = [null, null, null, null, null, null, null, null, null];
+	turn = 0;
+	user = 0;
+	gameboard.querySelectorAll('button').forEach(button => { button.style.backgroundImage = '';});
+	score.textContent = `${userscore[0]} - ${userscore[1]}`;
+}
+
+
+
+
