3400efcb16c26d82336f3233121aec451e4e0536
yhlas <wk.styhlas21@gmail.com>
2025-04-16 15:40:38 +0300

    Class objects added

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,54 +1,62 @@
-function createUser(name){
-	const username = name;
-	let score    = 0;
-	const addscore = ()=> score++;
-	const getscore = ()=> score;
-	const zeroscore = ()=>score=0;
-	
-	return {username, addscore, getscore, zeroscore};
-}
-
-function game(user1, user2){
-	let board = new Array(9).fill(null);
-	let gamestatus = false;
-	let turn       = 0;
-	const players  = [user1, user2];
-	
-	const schema = [
-		[0, 1, 2], [3, 4, 5], [6, 7, 8],
-		[0, 3, 6], [1, 4, 7], [2, 5, 8],
-		[0, 4, 8], [2, 4, 6]
-	];
-	
-	//const setStartingPlayer = (playerIndex) => turn = playerIndex;
-	
-	const markCell = (cell)=> board[cell] = turn % 2;
-	
-	const nextMove = (bool) => {
-		if(bool){turn +=1;}
-		else{turn = 0;}
-		return turn;
-	};
-	
-	const resetGame = ()=>{
-		board.fill(null);
-		gamestatus = false;
-		turn = 0;
+class User{
+	constructor(name){
+		this._username = name;
+		this._score    = 0;
 	}
 	
-	const isDraw = ()=>{
-		if(gamestatus === false && board.every((value)=>value!==null)){
+	addscore() { this._score++ }
+	zeroscore() { this._score=0}
+	
+	get name() {
+		return this._username;
+	}
+	
+	get score(){
+		return this._score;
+	}
+}
+
+class Game{
+	constructor(user1, user2){
+		this.board = new Array(9).fill(null);
+		this._gamestatus = false;
+		this.turn       = 0;
+		this.players  = [user1, user2];
+		
+		this.schema = [
+			[0, 1, 2], [3, 4, 5], [6, 7, 8],
+			[0, 3, 6], [1, 4, 7], [2, 5, 8],
+			[0, 4, 8], [2, 4, 6]
+		];
+	}
+	
+	markCell(cell) {this.board[cell] = this.turn % 2}
+	
+	nextMove(bool) {
+		if(bool){this.turn +=1;}
+		else{this.turn = 0;}
+		return this.turn;
+	}
+	
+	resetGame() {
+		this.board.fill(null);
+		this._gamestatus = false;
+		this.turn = 0;
+	}
+	
+	isDraw(){
+		if(this._gamestatus === false && this.board.every((value)=>value!==null)){
 			return true;
 		}
 		return false;
 	}
 	
-	const checkGame = () => {    
-	    for(const pattern of schema){
+	checkGame(){    
+	    for(const pattern of this.schema){
 	    	const [i, j, k] = pattern;
-		if (board[i] !== null && board[i] === board[j] && board[j] === board[k]) {
-			gamestatus = true;
-			players[(turn - 1) % 2].addscore();
+		if (this.board[i] !== null && this.board[i] === this.board[j] && this.board[j] === this.board[k]) {
+			this._gamestatus = true;
+			this.players[(this.turn - 1) % 2].addscore();
 			return;
 		}
 		
@@ -56,18 +64,15 @@ function game(user1, user2){
 	    return;
 	}
 	
-	return {
-		get gamestatus() { return gamestatus; }, 
-		markCell, board, checkGame, 
-		resetGame, nextMove, isDraw
-	}; 
+	get gamestatus() { return this._gamestatus; }
 }
 
-const user_1 = createUser(String(prompt("User 1 name")));
-const user_2 = createUser(String(prompt("User 2 name")));
-document.querySelector('.usernames').textContent = `${user_1.username} & ${user_2.username}`;
+const user_1 = new User(String(prompt("User 1 name")));
+const user_2 = new User(String(prompt("User 2 name")));
 
-const playBoard    = game(user_1, user_2);
+document.querySelector('.usernames').textContent = `${user_1.name} & ${user_2.name}`;
+
+const playBoard    = new Game(user_1, user_2);
 
 function reset(){
 	user_1.zeroscore();
@@ -75,9 +80,8 @@ function reset(){
 	playBoard.resetGame();
 	playBoard.nextMove(false);
 	document.querySelector('.gameboard').querySelectorAll('button').forEach(button => { button.style.backgroundImage = '';});
-	score.textContent = `${user_1.getscore()} - ${user_2.getscore()}`;
+	document.querySelector('.score').textContent = `${user_1.score} - ${user_2.score}`;
 }
-
 
 function play(button){
     const fired_button = parseInt(button.value);
@@ -93,7 +97,7 @@ function play(button){
 	
 	if(playBoard.gamestatus === true || playBoard.isDraw()){
 		document.querySelectorAll('button').forEach(button => { button.style.backgroundImage = '';});
-		document.querySelector('.score').textContent = `${user_1.getscore()} - ${user_2.getscore()}`;
+		document.querySelector('.score').textContent = `${user_1.score} - ${user_2.score}`;
 		playBoard.resetGame();
 	}
     }  
