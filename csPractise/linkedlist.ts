7859378428888200557aaafea3d4436d33df5d92
yhlas <wk.styhlas21@gmail.com>
2025-08-05 18:30:55 +0300

    write basic methods

--- csPractise/linkedlist.ts
+++ csPractise/linkedlist.ts
@@ -1,31 +1,120 @@
 class TheNode {
     value: string;
+    nextNode: TheNode | null;
+
     constructor(value: string = ''){
         this.value = value;
+        this.nextNode = null;
     }
-    nextNode: TheNode;
 };
 
 export class LinkedList {
-    headNode: TheNode = new TheNode();
+    headNode: TheNode;
+    constructor(value:string){
+        this.headNode = new TheNode(value);
+    }
     append(value: string): void {
-        this.headNode.nextNode = new TheNode(value);
+        let temp: TheNode = this.headNode;
+        while(temp.nextNode){
+            temp = temp.nextNode;
+        }
+        temp.nextNode = new TheNode(value);
     };
 
     prepend(value: string): void {
-        let tempNode = new TheNode(value);
+        let tempNode: TheNode = new TheNode(value);
         tempNode.nextNode = this.headNode;
         this.headNode = tempNode;
     };  
 
     size(): number {
-        let number: number = 0;
-        while (this.headNode.nextNode){
+        let number: number = 1;
+        let temp: TheNode = this.headNode;
+        while (temp.nextNode){
             number += 1;
+            temp = temp.nextNode;
         };
 
         return number;
     };
 
+    print(): void {
+        let temp: TheNode | null = this.headNode;
+        let output: string = '->';
+        while(temp){
+            output += "(" + temp.value + ")->";
+            temp = temp.nextNode;
+       }
+       console.log(output);
+    };
 
+    head(): TheNode {
+        return this.headNode;
+    };
+
+    tail(): TheNode {
+        let temp: TheNode = this.headNode;
+        while(temp.nextNode){
+            temp = temp.nextNode;
+        }
+
+        return temp;
+    };
+
+    at(index: number): TheNode {
+        let temp: TheNode = this.headNode;
+        if(index == 1){
+            return temp;
+        };
+        index -= 1;
+        while(temp.nextNode && index > 0){
+            temp = temp.nextNode;
+            index -= 1;
+        };
+
+        return temp;
+    };
+
+    pop(): void {
+        if(this.headNode.nextNode == undefined){
+            console.log("Nothing to pop!");
+            return;
+        };
+        let temp: TheNode = this.headNode;
+        let prev: TheNode | null = null;
+        while(temp.nextNode){
+            prev = temp;
+            temp = temp.nextNode;
+        };
+
+        if(prev){
+            prev.nextNode = null;
+        }
+    };
+
+    contains(value: string): boolean {
+        let temp: TheNode = this.headNode;
+
+        while(temp.value != value && temp.nextNode){
+            temp = temp.nextNode;
+        };
+        if(temp.nextNode == undefined && temp.value != value){
+            return false;
+        }
+        return true;
+    };
+
+    find(value: string): number | null{
+        let temp: TheNode = this.headNode;
+
+        let index: number = 1;
+        while(temp.nextNode){
+            if(temp.value === value){
+                return index
+            }
+            temp = temp.nextNode;
+            index += 1;
+        };
+        return null;
+    };
 };
--- csPractise/main.ts
+++ csPractise/main.ts
@@ -1,12 +1,36 @@
 import { LinkedList } from "./linkedlist";
 
-const list = new LinkedList();
+const list = new LinkedList("dog");
 
-list.append("dog");
 list.append("cat");
 list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
-console.log(list);
+list.print();
+
+list.prepend('horse');
+
+list.print();
+
+console.log("Size ", list.size());
+
+console.log("Head ", list.head());
+
+console.log("Tail ", list.tail());
+
+console.log("Node at 4: ", list.at(4));
+
+console.log("Contains ", "parrot", list.contains('parrot'));
+
+console.log("Contains ", "me", list.contains('me'));
+
+console.log("Find ", 'parrot ', list.find("parrot"));
+
+console.log("Find ", 'me ', list.find("me"));
+
+list.pop();
+
+list.print();
+
